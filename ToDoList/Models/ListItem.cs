0decfc1bd86c660a27c9897f926779ceafc60fc2
chrisfrear <chris.frear@hotmail.co.uk>
2025-07-09 22:19:11 +0100

    Create project and model

---
+++ ToDoList/Models/ListItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Models
+{
+    public class ListItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Completed { get; set; }
+    }
+}
